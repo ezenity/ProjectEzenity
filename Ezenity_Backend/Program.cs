c5c5e92a415c5492e86721ab59f62974e9fc29ae
DwDMadMac <anthonymmacallister@gmail.com>
2023-09-28 01:27:39 -0400

    feat(SwaggerUI): Enhance API description for JWT Authorization

--- Ezenity_Backend/Program.cs
+++ Ezenity_Backend/Program.cs
@@ -238,7 +238,7 @@ if (builder.Environment.IsDevelopment())
 
         setupAction.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
         {
-            Description = "JWT Authorization header using the Bear scheme (Example: 'Bearer 12345abcdef')",
+            Description = "JWT Authorization header using the Bearer scheme. Example: 'Bearer 12345abcdef'. Use this format when making authenticated API calls. You can obtain the JWT Bearer token by making a POST request to http://localhost:5000/api/v1/accounts/authenticate.",
             Name = "Authorization",
             In = ParameterLocation.Header,
             Type = SecuritySchemeType.Http,
