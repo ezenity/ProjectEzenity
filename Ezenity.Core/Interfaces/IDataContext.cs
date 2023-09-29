9dc6a590f08e08f084fb04ca56cb2fa2e30f8936
DwDMadMac <anthonymmacallister@gmail.com>
2023-09-29 16:20:28 -0400

    Feat: Added DataContext Interface
    - This will be utilized for unit testing and dependency injection

---
+++ Ezenity.Core/Interfaces/IDataContext.cs
@@ -0,0 +1,18 @@
+﻿using Ezenity.Core.Entities.Accounts;
+using Ezenity.Core.Entities.EmailTemplates;
+using Ezenity.Core.Entities.Sections;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ezenity.Core.Interfaces
+{
+    public interface IDataContext : IDisposable
+    {
+        DbSet<Account> Accounts { get; set; }
+        DbSet<Role> Roles { get; set; }
+        DbSet<EmailTemplate> EmailTemplates { get; set; }
+        DbSet<Section> Sections { get; set; }
+
+        int SaveChanges();
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
