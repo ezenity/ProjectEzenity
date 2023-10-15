d6824f3b4290a23ef86e404ea45e3a638fd5e3a8
DwDMadMac <anthonymmacallister@gmail.com>
2023-10-14 23:19:54 -0400

    Implement MockDbTransaction Class and Enhance AccountService Testing
    Full Details:
    - Added MockDbTransaction class for emulating IDbContextTransaction during unit tests.
      - Implemented asynchronous Commit and Rollback methods.
      - Provided logging to indicate when transaction methods are called.
      - Added XML summary comments for better documentation and IntelliSense support.
    
    - Modified AccountService class to use transactions.
      - Wrapped critical database operations under a transaction using `_context.BeginTransactionAsync()`.
    
    - Updated AccountServiceTests class to reflect changes in AccountService.
      - Adapted tests to account for the new transactional behavior.
      - Mocked IDataContext's `BeginTransactionAsync` method to return a new MockDbTransaction instance.
    
    Outcome:
    - Improved code reliability by using transactions for crucial database operations.
    - Enhanced unit testing to ensure that the new transactional logic behaves as expected.
    
    Affected Files:
    - Tests/Mocks/MockDbTransaction.cs
    - Core/Interfaces/IDataContext.cs
    - Infrastructure/Helpers/DataContext.cs
    - Services/AccountService.cs
    - Tests/AccountServiceTests.cs

--- Ezenity.Core/Interfaces/IAppSettings.cs
+++ Ezenity.Core/Interfaces/IAppSettings.cs
@@ -1,58 +1,64 @@
-﻿namespace Ezenity.Infrastructure.Helpers
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ezenity.Core.Interfaces
 {
     /// <summary>
     /// Contains settings for the application.
     /// </summary>
-    public class AppSettings
+    public interface IAppSettings
     {
         /// <summary>
         /// Gets the base URL of the application.
         /// </summary>
-        public string BaseUrl { get; }
+        string BaseUrl { get; }
 
         /// <summary>
         /// Gets the secret used for cryptographic operations.
         /// </summary>
-        public string Secret { get; }
+        string Secret { get; }
 
         /// <summary>
         /// Gets the Time-To-Live (TTL) for refresh tokens (in days).
         /// </summary>
-        public int RefreshTokenTTL { get; }
+        int RefreshTokenTTL { get; }
 
         /// <summary>
         /// Gets the email address that will be used as the 'From' address in outgoing emails.
         /// </summary>
-        public string EmailFrom { get; }
+        string EmailFrom { get; }
 
         /// <summary>
         /// Gets the host name for the SMTP server used for sending emails.
         /// </summary>
-        public string SmtpHost { get; }
+        string SmtpHost { get; }
 
         /// <summary>
         /// Gets the port number for the SMTP server.
         /// </summary>
-        public int SmtpPort { get; }
+        int SmtpPort { get; }
 
         /// <summary>
         /// Gets the username for authentication with the SMTP server.
         /// </summary>
-        public string SmtpUser { get; }
+        string SmtpUser { get; }
 
         /// <summary>
         /// Gets the password for authentication with the SMTP server.
         /// </summary>
-        public string SmtpPass { get; }
+        string SmtpPass { get; }
 
         /// <summary>
         /// Gets a value indicating whether SSL is enabled for SMTP communications.
         /// </summary>
-        public bool SmtpEnableSsl { get; }
+        bool SmtpEnableSsl { get; }
 
         /// <summary>
         /// Gets the access token used for some third-party services or internal processes.
         /// </summary>
-        public string AccessToken { get; }
+        string AccessToken { get; }
     }
 }
---
+++ Ezenity.Tests/Mocks/MockDbTransaction.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ezenity.Tests.Mocks
+{
+    /// <summary>
+    /// A mock implementation of IDbContextTransaction for unit testing purposes.
+    /// </summary>
+    public class MockDbTransaction : IDbContextTransaction
+    {
+        /// <summary>
+        /// Gets the transaction identifier.
+        /// </summary>
+        public Guid TransactionId { get; } = Guid.NewGuid();
+
+        /// <summary>
+        /// Simulates the Commit action for the transaction.
+        /// </summary>
+        public void Commit()
+        {
+            Console.WriteLine("Commit was called.");
+        }
+
+        /// <summary>
+        /// Simulates the asychronous Commit action for the transaction.
+        /// </summary>
+        /// <param name="cancellationToken">A token to observe while waiting for the task to complete.</param>
+        /// <returns></returns>
+        public Task CommitAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("CommitAsync was called.");
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// Disposes of the transaction resources.
+        /// </summary>
+        public void Dispose()
+        {
+            Console.WriteLine("Dispose was called.");
+        }
+
+        /// <summary>
+        /// Asynchronously disposes of the transation resources.
+        /// </summary>
+        /// <returns></returns>
+        public ValueTask DisposeAsync()
+        {
+            Console.WriteLine("DisposeAsync was called.");
+            return new ValueTask(Task.CompletedTask);
+        }
+
+        /// <summary>
+        /// Simulates the Rollback action for the transaction.
+        /// </summary>
+        public void Rollback()
+        {
+            Console.WriteLine("Rollback was called.");
+        }
+
+        /// <summary>
+        /// Simulates the asynchronous Rollback action for the transaction.
+        /// </summary>
+        /// <param name="cancellationToken">A token to observe while waiting for the task to complete.</param>
+        /// <returns></returns>
+        public Task RollbackAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("RollbackAsync was called.");
+            return Task.CompletedTask;
+        }
+    }
+}
