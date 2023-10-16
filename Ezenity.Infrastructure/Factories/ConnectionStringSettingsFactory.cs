b983d78805fda9ee6c8ebd85f3a8e1b33d436489
DwDMadMac <anthonymmacallister@gmail.com>
2023-10-16 19:49:58 -0400

    Refactor configuration loading and DI setup for AppSettings and ConnectionStrings
    This commit includes several changes aimed at improving the way AppSettings
    and ConnectionStrings are loaded and registered in the dependency injection
    container. The changes are as follows:
    
    - Created factory methods for creating instances of AppSettings and
      ConnectionStringSettings, encapsulating the loading logic.
    
    - Refactored Program.cs to utilize the new factory methods and simplified the
      registration process in the DI container.
    
    - Ensured that AppSettings and ConnectionStringSettings implement their
      respective interfaces, enabling better testability and adherence to SOLID
      principles.

---
+++ Ezenity.Infrastructure/Factories/ConnectionStringSettingsFactory.cs
@@ -0,0 +1,22 @@
+﻿using Ezenity.Infrastructure.Helpers;
+using Microsoft.Extensions.Configuration;
+
+namespace Ezenity.Infrastructure.Factories
+{
+    /// <summary>
+    /// Factory class responsible for creating instances of ConnectionStringSettings.
+    /// </summary>
+    public static class ConnectionStringSettingsFactory
+    {
+        /// <summary>
+        /// Creates an instance of ConnectionStringSettings based on the provided IConfiguration.
+        /// </summary>
+        /// <param name="configuration">The application configuration.</param>
+        /// <returns>A populated ConnectionStringSettings object.</returns>
+        public static ConnectionStringSettings Create(IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("WebApiDatabase");
+            return new ConnectionStringSettings(connectionString);
+        }
+    }
+}
---
+++ Ezenity.Infrastructure/Helpers/ConnectionStringSettingsWrapper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ezenity.Infrastructure.Helpers
+{
+    public class ConnectionStringSettingsWrapper
+    {
+        private readonly ConnectionStringSettings _connectionString;
+
+        public ConnectionStringSettingsWrapper(ConnectionStringSettings connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public string WebApiDatabase => _connectionString.WebApiDatabase;
+    }
+}
