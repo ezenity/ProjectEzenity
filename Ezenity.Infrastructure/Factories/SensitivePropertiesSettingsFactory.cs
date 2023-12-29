3f3b752b40cdacc089fd8b98d18789701f34a5bc
Anthony M MacAllister <2531816+ezenity@users.noreply.github.com>
2023-12-29 00:00:33 -0500

    Implement Configuration Settings and Wrappers for Enhanced Testability (#49)
    * Feat: Implement Configuration Settings and Wrappers for Enhanced Testability
    
    - Implemented AppSettings, ConnectionStringSettings, and SensitivePropertiesSettings wrappers for better abstraction and testability.
    - Corrected typo in the retrieval of 'SensitiveErrorProperties' from configuration.
    - Standardized the approach to accessing configuration settings across the application, ensuring consistency and readability.
    - Registered all settings with their respective wrappers in the DI container for easy mocking and extension.
    - Ensured all configuration sections are accessed correctly as per the structure of the 'appsettings.*.json' file.
    
    This commit improves the maintainability and testability of the configuration management and ensures adherence to Clean Architecture principles.
    
    * Refactor: Removed critical files from ignore list
    
    * Feat: Update Configuration Templates to Exclude Sensitive Data
    
    - Updated 'appsettings.template.json', 'appsettings.Development.template.json' and 'appsettings.Production.template.json' replacing sensitive connection strings with placeholders.
    - Documented the steps for developers to configure their local and production environments securely.
    - Ensured .gitignore is updated to prevent committing actual configuration files.
    
    This update ensures secure handling of sensitive database connection details and clear instructions for setup.
    
    * Chore: Add Documentation to ConnectionStringSettingsFactory
    
    - Added comprehensive summary comments to the ConnectionStringSettingsFactory class.
    - Documented the purpose and functionality of the Create method, explaining how it retrieves and wraps the 'WebApiDatabase' connection string.
    - Provided detailed parameter descriptions for enhanced clarity and developer guidance.
    - Ensured comments align with coding standards and provide clear insights into factory operations and its role in configuration management.
    
    This commit improves code readability and maintainability by providing essential documentation for the connection string settings creation process.
    
    * Fix: Error CS1026 During Build Script

---
+++ Ezenity.Infrastructure/Factories/SensitivePropertiesSettingsFactory.cs
@@ -0,0 +1,37 @@
+﻿using Ezenity.Core.Helpers.Exceptions;
+using Ezenity.Core.Interfaces;
+using Ezenity.Infrastructure.Helpers;
+using Microsoft.Extensions.Configuration;
+
+namespace Ezenity.Infrastructure.Factories
+{
+    /// <summary>
+    /// A factory class for creating instances of ISensitivePropertiesSettings. 
+    /// Retrieves the sensitive property settings from the application configuration and encapsulates them in a SensitivePropertiesSettingsWrapper.
+    /// </summary>
+    public static class SensitivePropertiesSettingsFactory
+    {
+        /// <summary>
+        /// Creates an instance of ISensitivePropertiesSettings based on the provided application configuration.
+        /// Retrieves the list of sensitive properties and wraps them for secure and centralized access.
+        /// </summary>
+        /// <param name="configuration">The application configuration containing the sensitive properties.</param>
+        /// <returns>An ISensitivePropertiesSettings implementation with sensitive properties initialized.</returns>
+        /// <exception cref="NotFoundException">Thrown if the 'SensitiveErrorProperties' section is missing in the configuration.</exception>
+        public static ISensitivePropertiesSettings Create(IConfiguration configuration)
+        {
+            // Get the sensitive proeprties list from the configuration
+            var sensitiveErrorProperties = configuration.GetSection("SensitiveErrorProperties").Get<List<string>>();
+
+            // Ensure senitive properties list from the configuration
+            if (sensitiveErrorProperties == null)
+            {
+                throw new NotFoundException("The 'SensitiveErrorProperties' section is missing in the configuration.");
+            }
+
+            // Create an instance of SensitiveProeprtiesConfiguration with the list
+            var sensitivePropertiesSettings = new SensitivePropertiesSettings(sensitiveErrorProperties);
+            return new SensitivePropertiesSettingsWrapper(sensitivePropertiesSettings);
+        }
+    }
+}
