3beb762de89c5cbe0fda479beb326da661764593
DwDMadMac <anthonymmacallister@gmail.com>
2024-01-02 21:48:07 -0500

    Demo Purposes
    Providing two front-end solutions that communicates with backend, will be expanding these two and refactoring to resolve the vulnerabilities.

---
+++ Ezenity_Trina/webpack.config.js
@@ -0,0 +1,51 @@
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.(png|jpe?g|gif|webp)?$/,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+      {
+        test: /\.less$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" },
+          { loader: "less-loader" },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    mainFiles: ["index", "Index"],
+    extensions: [".js", ".jsx"],
+    alias: {
+      "@": path.resolve(__dirname, "src/"),
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true,
+  },
+  externals: {
+    // global app config object
+    config: JSON.stringify({
+      apiUrl: "http://localhost:5000",
+    }),
+  },
+};
