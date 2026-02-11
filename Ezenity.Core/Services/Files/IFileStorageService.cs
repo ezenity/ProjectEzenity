c21f7cd617d67fce178d5b0a3a8eb3c348feb328
DwDMadMac <anthonymmacallister@gmail.com>
2026-02-11 02:29:42 -0500

    FIX: namespace

--- Ezenity.Core/Services/Files/IFileStorageService.cs
+++ Ezenity.Core/Services/Files/IFileStorageService.cs
@@ -3,7 +3,7 @@ using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
-using Ezenity.DTO.Models.Files;
+using Ezenity.DTOs.Models.Files;
 
 namespace Ezenity.Core.Services.Files
 {
